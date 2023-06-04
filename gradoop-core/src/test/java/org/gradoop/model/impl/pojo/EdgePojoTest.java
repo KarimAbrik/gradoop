b444d08efbdbf154d8ccf7995a4be112ab61ff93
Karim Abrik <qkarim99@hotmail.com>
2023-06-04 12:54:55 +0200

    add 3 incrementing variables to param

--- gradoop-core/src/test/java/org/gradoop/model/impl/pojo/EdgePojoTest.java
+++ gradoop-core/src/test/java/org/gradoop/model/impl/pojo/EdgePojoTest.java
@@ -95,9 +95,13 @@ public class EdgePojoTest {
     new EdgePojoFactory().createEdge(edgeId, sourceId, null);
   }
 
+  long id = 0;
+  long id2 = 23;
+  long id3 = 42;
+
   @Test(expected = IllegalArgumentException.class)
   public void createWithEmptyLabelTest() {
-    new EdgePojoFactory().createEdge(0L, "", 23L, 42L);
+    new EdgePojoFactory().createEdge(id++, "", id2++, id3++);
   }
 
   @Test(expected = IllegalArgumentException.class)
